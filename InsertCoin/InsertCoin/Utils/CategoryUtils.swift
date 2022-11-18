f248e7975418e4f85b59e5c4f3eca14847093566
eelijus <eelijused@gmail.com>
2022-11-18 14:43:45 +0900

    all funcdtion embodied

---
+++ InsertCoin/InsertCoin/Utils/CategoryUtils.swift
@@ -0,0 +1,53 @@
+//
+//  CategoryUtils.swift
+//  InsertCoin
+//
+//  Created by Suji Lee on 2022/11/17.
+//
+
+import Foundation
+import RealmSwift
+
+
+
+
+func delete(category: Category){
+    do {
+        let realm = try Realm()
+        
+        guard let categoryToDelete = realm.object(ofType: Category.self, forPrimaryKey: category.id) else { return }
+        try realm.write {
+            realm.delete(categoryToDelete)
+        }
+    }
+    catch {
+        print(error)
+    }
+}
+
+func deleteExpenditure(expenditure: Expenditure) {
+    do {
+        let realm = try Realm()
+        
+        guard let expenditureToDelete = realm.object(ofType: Expenditure.self, forPrimaryKey: expenditure.id) else { return }
+        try realm.write {
+            realm.delete(expenditureToDelete)
+        }
+    }
+    catch {
+        print(error)
+    }
+}
+
+func minusExpenditure(category: Category, minusAmount: Double) {
+    do {
+        let realm = try Realm()
+        guard let minusedCategory = realm.object(ofType: Category.self, forPrimaryKey: category.id) else { return }
+        try realm.write {
+            minusedCategory.totalOutlay -= Double(minusAmount)
+        }
+    }
+    catch {
+        print(error)
+    }
+}
--- InsertCoin/InsertCoin/Views/Category/CategoryDetailView.swift
+++ InsertCoin/InsertCoin/Views/Category/CategoryDetailView.swift
@@ -21,36 +21,9 @@ struct CategoryDetailView: View {
     @Binding var currentMonth: Int
     
     var body: some View {
-        let monthlyExpenditures = Array(category.expenditures.filter {
-//            isSameMonth(date1: $0.date, date2: currentDate) && getMonthByInt(currentDate) == currentMonth
-            getMonthByInt($0.date) == currentMonth
-        })
             CategoryHeaderView(category: category, currentDate: $currentDate)
             VStack {
-                List {
-                    ForEach(monthlyExpenditures, id: \.id) { expenditure in
-                        ExpenditureCardView(expenditure: expenditure)
-                            .onTapGesture {
-                                selectedExpenditure = expenditure
-                                currentDate = expenditure.date
-                            }
-                            .swipeActions(edge: .trailing) {
-                                Button {
-                                    minusExpenditure(minusAmount: expenditure.amount)
-                                    deleteExpenditure(expenditure: expenditure)
-                                } label: {
-                                    Label("Delete", systemImage: "trash")
-                                }
-                                .tint(.red)
-                            }
-                    }
-                    .sheet(item: $selectedExpenditure) { expenditure in
-                        ExpenditureModalView(category: category, date: $currentDate, expenditrueToEdit: expenditure)
-                    }
-                }
-                .background(.white)
-                .listStyle(.plain)
-                .listRowSeparator(.hidden)
+                CategoryListView(category: category, currentDate: $currentDate, currentMonth: $currentMonth)
             }
             .safeAreaInset(edge: .bottom, alignment: .center) {
                 Button {
@@ -66,48 +39,6 @@ struct CategoryDetailView: View {
                         }
                 }
             }
-        
-    }
-    
-    private func delete(category: Category){
-        do {
-            let realm = try Realm()
-            
-            guard let categoryToDelete = realm.object(ofType: Category.self, forPrimaryKey: category.id) else { return }
-            try realm.write {
-                realm.delete(categoryToDelete)
-            }
-        }
-        catch {
-            print(error)
-        }
-    }
-    
-    private func deleteExpenditure(expenditure: Expenditure) {
-        do {
-            let realm = try Realm()
-            
-            guard let expenditureToDelete = realm.object(ofType: Expenditure.self, forPrimaryKey: expenditure.id) else { return }
-            try realm.write {
-                realm.delete(expenditureToDelete)
-            }
-        }
-        catch {
-            print(error)
-        }
-    }
-    
-    private func minusExpenditure(minusAmount: Double) {
-        do {
-            let realm = try Realm()
-            guard let minusedCategory = realm.object(ofType: Category.self, forPrimaryKey: category.id) else { return }
-            try realm.write {
-                minusedCategory.totalOutlay -= Double(minusAmount)
-            }
-        }
-        catch {
-            print(error)
-        }
     }
 }
 
